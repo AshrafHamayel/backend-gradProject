451029e30e18e4da3236935e48075d7c5f18257f
AshrafHamayel <78316328+AshrafHamayel@users.noreply.github.com>
2022-07-06 08:46:18 +0300

    up

--- Pages/signUp.js
+++ Pages/signUp.js
@@ -6,10 +6,11 @@ const appl=express.Router();
 
 //appl.use(authenticate);///     2
 
-appl.get('/signUp',(req,res,err)=>{
+appl.get('/signUp',(req,res,err)=>
+{
 
 
-    res.json("fome Sign up")
+    res.json("fome Sign up !")
 
    //if (err)
    //res.json("pleas sigin")

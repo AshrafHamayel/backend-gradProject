7ca8910588c6af396748becd11b8bec045048845
AshrafHamayel <78316328+AshrafHamayel@users.noreply.github.com>
2022-07-16 01:15:25 +0300

    update signup

--- Pages/signUp.js
+++ Pages/signUp.js
@@ -6,46 +6,86 @@ const User =require('../models/user');
 
 
 
-let newUser=new User({
-name :'Ashraf',
-email:'asrf@gmail.com',
-password:'1234',
-image:'asd.png'
 
-});
 
 
 const appl=express.Router();
 
+
+var emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+
+
+function isEmailValid(email)
+ {
+    if (!email)
+        return false;
+
+    if(email.length>254)
+        return false;
+
+    var valid = emailRegex.test(email);
+    if(!valid)
+        return false;
+
+    // Further checking of some things regex can't handle
+    var parts = email.split("@");
+    if(parts[0].length>64)
+        return false;
+
+    var domainParts = parts[1].split(".");
+    if(domainParts.some(function(part) { return part.length>63; }))
+        return false;
+
+    return true;
+}
+
+
 //appl.use(authenticate);///     2
 
-appl.get('/signUp',(req,res,err)=>
+appl.get('/signUp',(req,res)=>
 {
 
 
-    res.json("fome Sign up !")
+var email = req.query.email;
 
 
+if (isEmailValid(email))
+{
+
+    var name = req.query.name;
+    var pass = req.query.password;
+    
+let newUser=new User(
+    {
+    name :name,
+    email:email,
+    password: pass,
+    image:'asd.png'
+    
+    });
 
 
     newUser.save( (err)=> {
         if(!err){
             console.log('done !');
         }
-        else {
-            console.log('error add !');
-        
+
+        else
+         {
+            console.log('Email exists !');
         
         }
         
         })
 
 
+}
 
+else{
+    res.send('Invalid Email !');
+  
+  }
 
-
-   //if (err)
-   //res.json("pleas sigin")
 
     });
 
